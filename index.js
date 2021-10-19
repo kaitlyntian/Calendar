327f832931f400ddbd780669353e4ec041b37cdb
Matthew Vargas <49174708+Automaton1989@users.noreply.github.com>
2021-10-18 20:56:47 -0700

    optimization of files

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
-app.use(express.static(__dirname + "/public/"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
