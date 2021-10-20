8287cae8285e4ab676737140cf9df9908861d691
Matthew Vargas <49174708+Automaton1989@users.noreply.github.com>
2021-10-20 11:22:53 -0700

    more index.html content updates (temp info), added linking to login.html, added register.html and linked it

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const router = express.Router();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
+
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+router.get("/login", function(req, res) {
+  res.sendFile(path.join(__dirname+"/public/login.html"));
+});
+
+router.get("/register", function(req, res) {
+  res.sendFile(path.join(__dirname+"/public/register.html"));
+});
+
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
