ec9e8aec3a239b8a5065e3fb3f60a40f7d6c62f3
Matthew Vargas <49174708+Automaton1989@users.noreply.github.com>
2021-10-24 19:04:42 -0700

    Db additions

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const port = 3000;
 const path = require("path");
 const router = express.Router();
 
+/* MONGODB CLIENT */
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "calendarproject";
+const client = new MongoClient(url);
+/* MONGDB END PRIMER PART */
+
 app.use(express.static(__dirname + "/public"));
 
 router.get("/", function(req, res) {
@@ -19,6 +26,17 @@ router.get("/register", function(req, res) {
 });
 
 app.use("/", router);
+
+/* MONGODB CONNECTION */ 
+client.connect(function(err) {
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+})
+
+/* MONGODB CLOSE CONNECTION */
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
